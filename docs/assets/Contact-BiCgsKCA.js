ef6d6be014decc1a3c5779e3b6426f1adbc7e84c
robinfrancis186 <robinfrancis186@gmail.com>
2026-02-23 20:35:05 +0530

    Revert "style: render Spline AI assistant natively with react-spline for true transparent background"
    This reverts commit aceeed29559b26127f4a4eb2c78b741589c68639.

--- docs/assets/Contact-BiCgsKCA.js
+++ docs/assets/Contact-BiCgsKCA.js
@@ -1,4 +1,4 @@
-import{r as $e,g as Ey,j as Pt,z as Ty,b as Ay,R as Cy,a as Nf}from"./index-DevYbEks.js";/**
+import{r as $e,g as Ey,j as Pt,z as Ty,b as Ay,R as Cy,a as Nf}from"./index-B87qGPlP.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
--- docs/assets/Projects-BOT9UZtT.js
+++ docs/assets/Projects-BOT9UZtT.js
@@ -1,4 +1,4 @@
-import{c as Zn,r as Y,j as A,a as Ae,m as $t}from"./index-DevYbEks.js";/**
+import{c as Zn,r as Y,j as A,a as Ae,m as $t}from"./index-B87qGPlP.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
